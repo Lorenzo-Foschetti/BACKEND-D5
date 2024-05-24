e1997e3bca6d0eb9a746d390173ce5b6f39e8067
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-05-24 12:38:52 +0200

    add methods for "luminosità"

--- src/Player/Immagine.java
+++ src/Player/Immagine.java
@@ -1,17 +1,42 @@
 package Player;
 
-public class Immagine extends Elementomultimediale implements Show {
+public class Immagine extends Elementomultimediale implements Show, Luminosità {
     private int luminosità;
 
     //costruttore
     public Immagine(String titolo) {
         super(titolo);
+        this.luminosità = luminosità;
     }
 
 
     //override del metodo dell'interfaccia show
     @Override
     public void show() {
+     
+    }
 
+    @Override
+    public void aumentaLuminosità() {
+        if (this.luminosità == 100) {
+            System.out.println("Non puoi alzare ulteriormente la luminosità");
+        } else this.setLuminosità(++luminosità);
+    }
+
+    @Override
+    public void diminuisciLuminosità() {
+        if (this.luminosità > 0) {
+            setLuminosità(--luminosità);
+        } else {
+            System.out.println("Non puoi abbassare ulteriormente la luminosità");
+        }
+    }
+
+    public int getLuminosità() {
+        return luminosità;
+    }
+
+    public void setLuminosità(int luminosità) {
+        this.luminosità = luminosità;
     }
 }
--- src/Player/Video.java
+++ src/Player/Video.java
@@ -1,6 +1,8 @@
 package Player;
 
-public class Video extends Elementomultimediale {
+import java.util.Random;
+
+public class Video extends Elementomultimediale implements Luminosità {
     private int luminosità;
     private int volume;
     private int durata;
@@ -9,5 +11,33 @@ public class Video extends Elementomultimediale {
     //costruttore
     public Video(String titolo) {
         super(titolo);
+        Random rndm = new Random();
+        this.luminosità = rndm.nextInt(1, 100);
+    }
+
+
+    //override dei metodi dell'interfaccia Luminosità
+    @Override
+    public void aumentaLuminosità() {
+        if (this.luminosità == 100) {
+            System.out.println("Non puoi alzare ulteriormente la luminosità");
+        } else this.setLuminosità(++luminosità);
+    }
+
+    @Override
+    public void diminuisciLuminosità() {
+        if (this.luminosità > 0) {
+            setLuminosità(--luminosità);
+        } else {
+            System.out.println("Non puoi abbassare ulteriormente la luminosità");
+        }
+    }
+
+    public int getLuminosità() {
+        return luminosità;
+    }
+
+    public void setLuminosità(int luminosità) {
+        this.luminosità = luminosità;
     }
 }
